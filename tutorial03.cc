d4300abd5b67eb2b470d10d6840d83a4983e7b52
jeongyongkim <rlawjddyd300@gmail.com>
2020-07-17 08:36:29 +0900

    tutorial3 update

--- tutorial03.cc
+++ tutorial03.cc
@@ -40,6 +40,8 @@ typedef struct PacketQueue {
 } PacketQueue;
 
 PacketQueue audioq;
+SDL_AudioSpec spec;
+SwrContext* swr_ctx;
 int quit = 0;
 
 void packet_queue_init(PacketQueue *q) {
@@ -52,7 +54,6 @@ int packet_queue_put_private(PacketQueue *q, AVPacket *pkt)
 {
     AVPacketList *pkt1;
 
-    printf("queue put private \n");
     pkt1 = (AVPacketList*)av_malloc(sizeof(AVPacketList));
     if (!pkt1)
         return -1;
@@ -82,7 +83,6 @@ int packet_queue_put(PacketQueue *q, AVPacket *pkt)
 }
 
 int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block) {
-    printf("get queue \n");
     AVPacketList *pkt1;
     int ret;
 
@@ -90,7 +90,7 @@ int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block) {
     for (;;) {
         pkt1 = q->first_pkt;
         if (pkt1) {
-            printf("get queue %d\n", __LINE__);
+            printf("get packet %d\n", __LINE__);
             q->first_pkt = pkt1->next;
             if (!q->first_pkt)
                 q->last_pkt = NULL;
@@ -101,7 +101,6 @@ int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block) {
             ret = 1;
             break;
         } else if (!block) {
-            printf("get queue %d", __LINE__);
             ret = 0;
             break;
         } else {
@@ -109,10 +108,71 @@ int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block) {
             SDL_CondWait(q->cond, q->mutex);
         }
     }
-    cout << "get packet : " << pkt <<endl;
     SDL_UnlockMutex(q->mutex);
-    printf("pkt size : %d \n", pkt->size);
     return ret;
+}
+
+uint8_t audio_buf1[(MAX_AUDIO_FRAME_SIZE * 3) / 2];
+
+int converting(AVCodecContext *aCodecCtx, AVFrame* frame) {
+  int data_size = av_samples_get_buffer_size(NULL,
+      aCodecCtx->channels,
+      frame->nb_samples,
+      aCodecCtx->sample_fmt,
+      1);
+
+  auto dec_channel_layout =
+    (frame->channel_layout && frame->channels == av_get_channel_layout_nb_channels(frame->channel_layout)) ?
+    frame->channel_layout : av_get_default_channel_layout(frame->channels);
+
+  if (frame->format != AV_SAMPLE_FMT_S16 ||
+      dec_channel_layout != aCodecCtx->channel_layout ) {
+    swr_ctx = swr_alloc_set_opts(NULL,
+        aCodecCtx->channel_layout, (AVSampleFormat)AV_SAMPLE_FMT_S16, spec.freq,
+        dec_channel_layout,        (AVSampleFormat)frame->format,     frame->sample_rate,
+        0, NULL);
+
+    if (!swr_ctx || swr_init(swr_ctx) < 0) {
+      swr_free(&swr_ctx);
+      return -1;
+    }
+  }
+
+  int resampled_data_size;
+  if (swr_ctx) {
+    const uint8_t **in = (const uint8_t **)frame->extended_data;
+    int out_count =  + 256;
+    int out_size  = av_samples_get_buffer_size(NULL, spec.channels, out_count, AV_SAMPLE_FMT_S16, 0);
+    int len2;
+    if (out_size < 0) {
+      av_log(NULL, AV_LOG_ERROR, "av_samples_get_buffer_size() failed\n");
+      return -1;
+    }
+    /* if (wanted_nb_samples != af->frame->nb_samples) {
+     *   if (swr_set_compensation(is->swr_ctx, (wanted_nb_samples - af->frame->nb_samples) * is->audio_tgt.freq / af->frame->sample_rate,
+     *         wanted_nb_samples * is->audio_tgt.freq / af->frame->sample_rate) < 0) {
+     *     av_log(NULL, AV_LOG_ERROR, "swr_set_compensation() failed\n");
+     *     return -1;
+     *   }
+     * } */
+    /* av_fast_malloc(&audio_buf1, &audio_buf1_size, out_size); */
+    if (!audio_buf1)
+      return AVERROR(ENOMEM);
+    len2 = swr_convert(swr_ctx, &audio_buf1, out_count, in, frame->nb_samples);
+    if (len2 < 0) {
+      av_log(NULL, AV_LOG_ERROR, "swr_convert() failed\n");
+      return -1;
+    }
+    if (len2 == out_count) { av_log(NULL, AV_LOG_WARNING, "audio buffer is probably too small\n");
+      if (swr_init(swr_ctx) < 0)
+        swr_free(&swr_ctx);
+    }
+    audio_buf = audio_buf1;
+    resampled_data_size = len2 * spec.channels * av_get_bytes_per_sample(AV_SAMPLE_FMT_S16);
+  } else {
+    is->audio_buf = af->frame->data[0];
+    resampled_data_size = data_size;
+  }
 }
 
 int audio_decode_frame(AVCodecContext *aCodecCtx, uint8_t *audio_buf, int buf_size) {
@@ -126,48 +186,44 @@ int audio_decode_frame(AVCodecContext *aCodecCtx, uint8_t *audio_buf, int buf_si
   int len1, data_size = 0;
 
   for(;;) {
-    /* while(audio_pkt_size > 0) { */
-      // int got_frame = 0;
-      /* int frameFinished = 0;
-       * len1 = avcodec_decode_audio4(aCodecCtx, frame, &frameFinished, pkt); */
-      // len1 = avcodec_send_packet(aCodecCtx, pkt);
-      // cout << "len1 : " <<len1 << endl;
-      // bool frameFinished = 0;
-      // if (!(len1 < 0 && len1 != AVERROR(EAGAIN) && len1 != AVERROR_EOF)) {
-      //   if (len1 >= 0)
-      //     pkt->size = 0;
-      //   cout << "receive packet !! " << endl;
-      //   len1 = avcodec_receive_frame(aCodecCtx, frame);
-      //   if (len1 >= 0) {
-      //     frameFinished = 1;
-      //     cout << "got frame !!" << endl;
-      //   }
-      // }
-      //
-      // if(frameFinished)
-      //   continue;
-      // audio_pkt_data += len1;
-      // audio_pkt_size -= len1;
-      // data_size = 0;
-      // if(got_frame) {
-      //   data_size = av_samples_get_buffer_size(NULL,
-      //       aCodecCtx->channels,
-      //       frame->nb_samples,
-      //       aCodecCtx->sample_fmt,
-      //       1);
-      //   assert(data_size <= buf_size);
-      //   memcpy(audio_buf, frame->data[0], data_size);
-      // }
-      // if(data_size <= 0) {
-      //   [> No data yet, get more frames <]
-      //   continue;
-      // }
-      // [> We have data, return it and come back for more later <]
+    while(audio_pkt_size > 0) {
+      len1 = avcodec_send_packet(aCodecCtx, pkt);
+      bool frameFinished = 0;
+      if (!(len1 < 0 && len1 != AVERROR(EAGAIN) && len1 != AVERROR_EOF)) {
+        if (len1 >= 0)
+          pkt->size = 0;
+        cout << "receive packet !! " << endl;
+        len1 = avcodec_receive_frame(aCodecCtx, frame);
+        if (len1 >= 0) {
+          frameFinished = 1;
+          cout << "got frame !!" << endl;
+        }
+      }
+
+
+      data_size = 0;
+      if(frameFinished) {
+        data_size = av_samples_get_buffer_size(NULL,
+            aCodecCtx->channels,
+            frame->nb_samples,
+            aCodecCtx->sample_fmt,
+            1);
+        assert(data_size <= buf_size);
+        memcpy(audio_buf, frame->data[0], data_size);
+      }
+
+      audio_pkt_data += data_size;
+      audio_pkt_size -= data_size;
+      if(data_size <= 0) {
+        // No data yet, get more frames
+        continue;
+      }
+      // We have data, return it and come back for more later
       // av_frame_free(&frame);
-      // return data_size;
-    /* } */
-    /* if(pkt->data)
-     *   av_packet_unref(pkt); */
+      return data_size;
+    }
+    if(pkt->data)
+      av_packet_unref(pkt);
 
     if(quit) {
       av_frame_free(&frame);
@@ -183,19 +239,19 @@ int audio_decode_frame(AVCodecContext *aCodecCtx, uint8_t *audio_buf, int buf_si
     audio_pkt_data = pkt->data;
     audio_pkt_size = pkt->size;
 
-    len1 = avcodec_send_packet(aCodecCtx, pkt);
-    cout << "len1 : " <<len1 << endl;
-    bool frameFinished = 0;
-    if (!(len1 < 0 && len1 != AVERROR(EAGAIN) && len1 != AVERROR_EOF)) {
-      if (len1 >= 0)
-        pkt->size = 0;
-      cout << "receive packet !! " << endl;
-      len1 = avcodec_receive_frame(aCodecCtx, frame);
-      if (len1 >= 0) {
-        frameFinished = 1;
-        cout << "got frame !!" << endl;
-      }
-    }
+    /* len1 = avcodec_send_packet(aCodecCtx, pkt);
+     * bool frameFinished = 0;
+     * if (!(len1 < 0 && len1 != AVERROR(EAGAIN) && len1 != AVERROR_EOF)) {
+     *   if (len1 >= 0)
+     *     pkt->size = 0;
+     *   cout << "receive packet !! " << endl;
+     *   len1 = avcodec_receive_frame(aCodecCtx, frame);
+     *   if (len1 >= 0) {
+     *     cout << "len1 : " <<len1 << endl;
+     *     frameFinished = 1;
+     *     cout << "got frame !!" << endl;
+     *   }
+     * } */
   }
   av_frame_free(&frame);
 }
@@ -240,7 +296,7 @@ void audio_callback(void *userdata, Uint8 *stream, int len) {
 }
 
 int main(int argc, char *argv[]) {
-  /* av_log_set_level(AV_LOG_TRACE); */
+  av_log_set_level(AV_LOG_DEBUG);
   AVCodecContext *aCodecCtx;
 
   AVFormatContext *pFormatCtx = NULL;
@@ -256,7 +312,8 @@ int main(int argc, char *argv[]) {
   SDL_Window *screen;
   SDL_Renderer *renderer;
   SDL_Texture *texture;
-  SDL_AudioSpec   wanted_spec, spec;
+  /* SDL_AudioSpec   wanted_spec, spec; */
+  SDL_AudioSpec   wanted_spec;
   Uint8 *yPlane, *uPlane, *vPlane; size_t yPlaneSz, uvPlaneSz;
   int uvPitch;
 
@@ -318,6 +375,7 @@ int main(int argc, char *argv[]) {
   SDL_memset(&wanted_spec, 0, sizeof(wanted_spec)); /* or SDL_zero(want) */
   wanted_spec.freq = aCodecCtx->sample_rate;
   wanted_spec.format = AUDIO_S16SYS;
+  /* wanted_spec.format = AUDIO_F32SYS; */
   /* av_get_channel_layout_nb_channels(aCodecCtx->channels); */
   wanted_spec.channels = aCodecCtx->channels;
   cout << "aCodecCtx channel : " << aCodecCtx->channels << endl;
@@ -330,7 +388,9 @@ int main(int argc, char *argv[]) {
   if (dev == 0) {
     exit(-1);
   } else {
-    if (wanted_spec.format != spec.format) { /* we let this one thing change. */
+    if (wanted_spec.format != spec.format) {
+      /* we let this one thing change. */
+      cout << "format is different" << endl;
     }
   }
   cout << "wanted spec channel : " <<  wanted_spec.channels << endl;
--- tutorial03.h
+++ tutorial03.h
@@ -2,6 +2,7 @@ extern "C"{
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_thread.h>
