b10869557c0552f69c896ddf893edccaeb40cdf2
jeongyongkim <rlawjddyd300@gmail.com>
2021-09-05 23:39:20 +0900

    [feat] add decoder

---
+++ decoder/decoder.h
@@ -0,0 +1,23 @@
+#ifndef DECODER 
+#define DECODER 
+
+extern "C" {
+#include <libavformat/avformat.h>
+}
+#include <string>
+
+class Decoder{
+public:
+  Decoder(int stream_index, std::shared_ptr<AVFormatContext>& fmt_ctx);
+  ~Decoder();
+  void init();
+  bool getFrame(std::shared_ptr<AVPacket>& packet, std::shared_ptr<AVFrame>& frame);
+  int getIndex() {return stream_index_;}
+private:
+  int stream_index_;
+  std::shared_ptr<AVCodecContext> codec_context_;
+  std::shared_ptr<AVFrame> pFrame;
+  std::shared_ptr<AVFormatContext> fmt_ctx_;
+};
+
+#endif // DECODER
