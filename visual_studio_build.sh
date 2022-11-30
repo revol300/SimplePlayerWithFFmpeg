397cc8bebd30c6c0a881effac21d8c2e847ad389
JeongYong Kim <rlawjddyd300@gmail.com>
2022-12-01 03:00:40 +0900

    [fix] sh 파일 삭제

--- visual_studio_build.sh
+++
@@ -1,8 +0,0 @@
-PROJECT_PATH=$(git rev-parse --show-toplevel)
-echo ${PROJECT_PATH}
-
-mkdir -p ${PROJECT_PATH}/VisualStudio
-pushd VisualStudio
-  conan install ${PROJECT_PATH}/conan/conanfile.txt -pr=playerProfile --build=missing --update
-  cmake ../
-popd
