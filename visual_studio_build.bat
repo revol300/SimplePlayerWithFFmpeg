65bf353fd6211e16b78d5a8a5b9af55e2ae868ed
JeongYong Kim <rlawjddyd300@gmail.com>
2022-12-02 01:49:43 +0900

    [feat] batch file update

--- visual_studio_build.bat
+++ visual_studio_build.bat
@@ -1,7 +1,7 @@
 for /f %%i in ('git rev-parse --show-toplevel')   do set PROJECT_PATH=%%i
 
-mkdir -p %PROJECT_PATH%/VisualStudio
+mkdir -p VisualStudio
 pushd VisualStudio
   conan install %PROJECT_PATH%/conan/conanfile.txt -pr=playerProfile --build=missing --update
-  cmake ../
+  cmake ..\
 popd
